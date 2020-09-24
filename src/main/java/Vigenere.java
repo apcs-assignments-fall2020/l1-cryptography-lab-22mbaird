385d51793dffc4452cf8b9e2de5bd27513e43eb7
22mbaird <22mbaird@athenian.org>
2020-09-24 17:08:39 +0000

    Added both methods, I couldnt quite figure it out

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -1,14 +1,46 @@
 import java.util.Scanner;
 
 public class Vigenere {
-    public static String encryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+    public static String encryptVigenere(String message, int key) {
+        String str1="";
+        String stc ="";
+        
+        for (int i = 0; i<message.length(); i++){
+            char cif = message.charAt(i);
+            if(97<=cif && cif<=122){
+                int c=(int)(cif-97);
+                stc = String.valueOf(c);
+            }
+            else {
+                break;
+            }
+
+            
+            
+            
+            str1=str1+stc;
+       }
+        return str1;
     }
 
-    public static String decryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+    public static String decryptVigenere(String message, int key) {
+        String str1="";
+        String stc ="";
+        
+        for (int i = 0; i<message.length(); i++){
+            int ci = message.charAt(i);
+            if(0<=ci && ci<=25){
+                int c=(int)(ci+97);
+                stc = String.valueOf(c);
+            }
+            else{
+                break;
+            }
+            
+            
+            str1=str1+stc;
+       }
+        return str1;
     }
 
 
@@ -22,7 +54,7 @@ public class Vigenere {
             System.out.println("Please enter your message to be encrypted: ");
             String message = scan.nextLine();
             System.out.println("Please enter the key for your message: ");
-            String key = scan.nextLine().trim().toUpperCase();
+            Integer key = scan.nextInt();
             System.out.println("Here is your encrypted message: ");
             System.out.println(encryptVigenere(message, key));
         }
@@ -30,7 +62,7 @@ public class Vigenere {
             System.out.println("Please enter your message to be decrypted: ");
             String message = scan.nextLine();
             System.out.println("Please enter the key for your message: ");
-            String key = scan.nextLine().trim().toUpperCase();
+            Integer key = scan.nextInt();
             System.out.println("Here is your decrypted message: ");
             System.out.println(decryptVigenere(message, key));
         }
